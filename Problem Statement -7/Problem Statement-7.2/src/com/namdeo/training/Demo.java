962a43d1cf42320c600339977865241edbd32c3d
RashiNamdeo <97430918+RashiNamdeo@users.noreply.github.com>
2022-03-16 19:46:40 +0530

    Add files via upload

---
+++ Problem Statement -7/Problem Statement-7.2/src/com/namdeo/training/Demo.java
@@ -0,0 +1,9 @@
+package com.namdeo.training;
+
+class NameNotValidException extends Exception
+{
+     public String validname()
+     {
+          return ("Name is not Valid..Please ReEnter the Name");
+     }
+}
